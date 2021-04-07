d223a3e8dc2c4113f2b7d0013c06d4985dee6929
vivalvia <66685046+vivalvia@users.noreply.github.com>
2021-04-07 09:56:09 +0700

    E31190785

---
+++ Minggu9/application/models/Hello_model.php
@@ -0,0 +1,12 @@
+<?php
+class Hello_model extends CI_Model{
+    //public $txt = 'Hello World!';
+    
+    public $alert = 'Hello Wolrd dari CI Model';
+    
+    public $mahasiswa = [
+			["nama"=>"Adya Zalfa","nim"=>"E31190957","gol"=>"B"],
+			["nama"=>"Vivalvia ","nim"=>"E31190785","gol"=>"A"],
+			["nama"=>"Shindi Fatikah","nim"=>"E31191120","gol"=>"B"] 
+		];
+}
---
+++ Minggu9/application/models/M_coba.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_coba extends CI_Model
+{
+    public $alert = 'Hello Wolrd dari CI Model';
+}
